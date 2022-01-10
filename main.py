d8f67dd376412fb6158161cd556d1584b5ea0110
Cheng Yan <yancheng0930@gmail.com>
2022-01-10 13:21:38 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+import numpy as np
+import openai
+
+# upload pre-defined files on openai (only once)
+
+
+# exact the text in BPMN
+
+
+# upload the text in BPMN as the query for searching
+
+
+# get the semantic search results by GPT-3
+
+
+
+# Determine the skill group function corresponding to the primitive and its input parameters
+
+
+
+# output the robot's ROS programming codes
+
+
+

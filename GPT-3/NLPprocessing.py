89a02627db63161c29ea47359ab7d70faf2cea8c
Cheng Yan <yancheng0930@gmail.com>
2022-01-19 22:26:43 +0100

    Update NLPprocessing.py

--- GPT-3/NLPprocessing.py
+++ GPT-3/NLPprocessing.py
@@ -1,7 +1,7 @@
 ﻿import numpy as np
 import re
 import openai
-openai.api_key = ""      # enter your API for GPT-3 here
+openai.api_key = "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"      # enter your API for GPT-3 here
 import json
 
 
@@ -10,7 +10,7 @@ def nlp_search_results(query, file_ID):
     use GPT-3 model online-application to get the semantic search results
 
     :param query: the search content, such as: query = "grasp object"
-    :param file_ID: pre-defined file library () "file-xJ3fKj2VDatppSg23ZxZb4oI"
+    :param file_ID: pre-defined file library
     :return: search response of GPT-3
     """
     search_response = openai.Engine("davinci").search(
@@ -39,9 +39,24 @@ def best_score(search_response):
     return max_score
 
 
+def corr_text(search_response):
+    """
+    output the pre-defined content that correspond to the query in search response
+
+    :param search_response: search result of GPT-3
+    :return: the corresponding command text in pre-defined files
+    """
+    string = str(search_response)
+    dict = eval(string)
+    for i in dict["data"]:
+        data_dict = i
+    text = data_dict["text"]
+    return text
+
+
 def best_command(search_response):
     """
-    output the pre-defined content that get the best score in search response
+    output the pre-defined content that get the best score in search response(some problem)
 
     :param search_response: search result of GPT-3
     :return: the corresponding command text in pre-defined files
@@ -65,16 +80,18 @@ data = json.load(BPMN)
 # get all action names
 for i in data["activities"]:
     # get each search result by GPT-3
-    search_response = nlp_search_results(i['act_name'], "file-xJ3fKj2VDatppSg23ZxZb4oI")
+    search_response = nlp_search_results(i['act_name'], "file-kH1toOuo8l1SEfVR1mda5ZTP")
     # get corresponding command as primitive
     dict = i
-    dict["primitive"] = best_command(search_response)
+    dict["primitive"] = corr_text(search_response)
 
     # write the corresponding "primitive" in to BPMN file.json
     # json.dump(dict)
 
-    # print all
-    print(dict)
+    # print user's input sentence
+    print(i["act_name"])
+    # print primitive
+    print(dict["primitive"])
 
 # Closing file
 BPMN.close()

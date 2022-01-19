9172b62acb4cee369f35a4ce3b95916d8e523360
Cheng Yan <yancheng0930@gmail.com>
2022-01-19 22:09:10 +0100

    Provides 2 predefined files: skill & primitive

--- GPT-3/init/init.py
+++ GPT-3/init/init.py
@@ -1,14 +1,18 @@
 import openai
 
 # enter your API
-openai.api_key = "your API"
+openai.api_key = "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"
 
 # upload your file library
-response = openai.File.create(file=open("predefined.jsonl"), purpose="search")
+response = openai.File.create(file=open("predefined skill.jsonl"), purpose="search")
 
 # get your file ID as response
 print(response)
 
 # then your can use this ID in the main.py
 
-# "file-aVmfougPtrAX3RA40qSufHqQ"
+# "filename": "predefined primitive.jsonl"
+# "id": "file-kH1toOuo8l1SEfVR1mda5ZTP"
+
+# "filename": "predefined skill.jsonl"
+# "id": "file-azrvWHleyJ83Dov3GcC2pKrp"
